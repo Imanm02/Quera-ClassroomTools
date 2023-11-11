f60dd5ad4fcd5f89059c6041fe2526e72fc49e81
Imanm02 <imanm1381@gmail.com>
2023-11-11 13:56:56 +0800

    update code

--- score_compiler.py
+++ score_compiler.py
@@ -2,30 +2,39 @@ import os
 import re
 import pandas as pd
 
+# Load student IDs from Excel
 df = pd.read_excel('students.xlsx', header=None)
-questions = []
 students = df[0].tolist()
 
+# Initialize questions and scores
+questions = []
+scores = {}
+
+# Walk through directories to find question folders and initialize scores
 for path, dirs, files in os.walk('.'):
     questions = dirs
+    scores = {question: {student: 0 for student in students} for question in questions}
     break
-scores = dict([(question, dict([(student, 0) for student in students])) for question in questions])
-for i, (path, dirs, files) in enumerate(os.walk('.')):
+
+# Process each file and extract scores
+for path, dirs, files in os.walk('.'):
     if len(path.split('\\')) < 3:
         continue
     _, q, s = path.split('\\')
     try:
         s = int(s)
-    except:
-        # For those who haven't entered their student number (actually haven't submitted any number)
-        pass
+    except ValueError:
+        # Skip if student number is invalid
+        continue
     if s not in students:
         continue
-    with open(f'{path}\\result.txt', encoding="utf8") as EnteringQueraScores:
-        res = EnteringQueraScores.read()
+    with open(f'{path}\\result.txt', encoding="utf8") as file:
+        res = file.read()
         match = re.search(r'judge score with delay: (\d+)', res)
         if match:
             scores[q][s] = int(match.group(1))
+
+# Convert scores to DataFrame and join with student IDs
 df2 = pd.DataFrame(scores)
 df = df.join(df2, on=[0])
 df.to_excel('Results.xlsx')
