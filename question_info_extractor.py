02aabe86d90eca0d1aa8053a76982bf4e1d17bf0
Imanm02 <imanm1381@gmail.com>
2023-11-11 14:03:07 +0800

    update code

--- question_info_extractor.py
+++ question_info_extractor.py
@@ -4,19 +4,21 @@ import re
 
 session = requests.Session()
 cookies = {'session_id': '31jzn0j0xbrtbl48ioab2qoxicr0ls71'}
-text = requests.get('https://quera.org/course/6600/', cookies=cookies).text
-soup = BeautifulSoup(text, features='html.parser')
-rows = soup.find_all('div', attrs={'class': 'menu'})[0].find_all('a')
+response = session.get('https://quera.org/course/6600/', cookies=cookies).text
+soup = BeautifulSoup(response, features='html.parser')
+
+# Extract problem set information
 problemsets = []
-for row in rows:
+for row in soup.find_all('div', attrs={'class': 'menu'})[0].find_all('a'):
     problemsets.append({'title': row['data-text'].strip(),
                         'link': 'https://quera.org' + row['href'].splitlines()[0].strip()})
+
+# Extract question information from each problem set
 for problemset in problemsets:
-    text = requests.get(problemset['link'], cookies=cookies).text
-    soup = BeautifulSoup(text, features='html.parser')
-    qs = soup.find_all('a', attrs={'class': 'item problem_menu_item'})
-    for q in qs:
+    response = session.get(problemset['link'], cookies=cookies).text
+    soup = BeautifulSoup(response, features='html.parser')
+    for q in soup.find_all('a', attrs={'class': 'item problem_menu_item'}):
         params = re.sub(' +', ' ', q.text).splitlines()
-        name = params[2]
-        score = params[-2]
-        print(f"{name}, {problemset['title']},  https://quera.ir{q['href'].splitlines()[0].strip()}, کصشر, {score}")
+        name = params[2].strip()
+        score = params[-2].strip()
+        print(f"{name}, {problemset['title']}, https://quera.ir{q['href'].splitlines()[0].strip()}, {score}")
