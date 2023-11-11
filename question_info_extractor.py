add85339888a9e634220015384206ebd7672bceb
Imanm02 <imanm1381@gmail.com>
2023-11-11 13:49:56 +0800

    update question_info_extractor

--- question_info_extractor.py
+++ question_info_extractor.py
@@ -8,7 +8,7 @@ with open('data.csv', 'w', encoding='utf-8', newline='') as f:
     session = requests.Session()
     page_count = 26
     for count in range(1, page_count + 1):
-        text = session.get(f"https://quera.org/overview/course/3657/manage/edit_user/?page={count}", cookies= {'session_id': '47n484icce2onlj0zge5tvjffth5lbxq'}).text
+        text = session.get(f"https://quera.org/overview/course/3657/manage/edit_user/?page={count}", cookies= {'session_id': ''}).text
         soup = BeautifulSoup(text, features="html.parser")
         rows = soup.tbody.find_all('tr')
         for row in rows:
